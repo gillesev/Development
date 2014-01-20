7be2a35b5fcc69d3aba5def47e1cf0b0e647d41b
gillesev <gillesev@gmail.com>
2014-01-20 09:41:34 -0800

    IO Processor

---
+++ IO/IIOProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO
+{
+    public interface IIOProcessor : IDisposable
+    {
+        Task<string> Send(string msg, int timeout);
+    }
+}
