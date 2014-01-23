b684439c136c60cf1679d8e1a0f91103f9663edd
Gilles Eveloy <gillesev@gmail.com>
2014-01-23 15:01:30 -0800

    Update IIOSyncNonBlockingUnitProcessor.cs

--- IIOSyncNonBlockingUnitProcessor.cs
+++ IIOSyncNonBlockingUnitProcessor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace E3Retail.TP.HostAuthAdapter.AJB.Comm.FiPay20
+namespace IO
 {
     /// <summary>
     /// Elementary component able to process a IO message in a synchronous non blocking pattern.
