680d7c22ac75e3ddcde3700b5c6306925aa855ae
Gilles Eveloy <gillesev@gmail.com>
2014-01-23 15:01:05 -0800

    Update IIODispatcher.cs

--- IIODispatcher.cs
+++ IIODispatcher.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace E3Retail.TP.HostAuthAdapter.AJB.Comm.FiPay20
+namespace IO
 {
     /// <summary>
     /// Dispatches a message to an underlying IO processor infrastructure
